4d3520e3acb168ed4107e8fb91670317bc3441a1
maurolibanore <maurolibanore10@gmail.com>
2026-08-03 16:58:26 -0300

    feat: ajustando as rotas com o AuthProvider e as proteções de rota

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
-// Importando a sua tela de cadastro
+import { AuthProvider } from './context/AuthContext';
+import RotaProtegida from './components/RotaProtegida';
 import Cadastro from './pages/Cadastro/Cadastro'; 
 
-// Quando você criar o arquivo Login.jsx, é só descomentar as linhas abaixo:
 // import Login from './pages/Login/Login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Redireciona a página inicial direto para o cadastro para você testar */}
-        <Route path="/" element={<Navigate to="/cadastro" />} />
-        
-        {/* Rota da tela de cadastro */}
-        <Route path="/cadastro" element={<Cadastro />} />
-        
-        {/* Rota da tela de login (descomente quando criar) */}
-        {/* <Route path="/login" element={<Login />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Redireciona a página inicial direto para o cadastro para você testar */}
+          <Route path="/" element={<Navigate to="/cadastro" />} />
+          
+          {/* Rota da tela de cadastro */}
+          <Route path="/cadastro" element={<Cadastro />} />
+          
+          {/* Rota da tela de login (descomente quando criar) */}
+          {/* <Route path="/login" element={<Login />} /> */}
+        </Routes>
+      </BrowserRouter>      
+    </AuthProvider>
+
   );
 }
 
