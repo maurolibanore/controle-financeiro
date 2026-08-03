e9501b499c8bd89ad1cb80e40bd71f5956def3cd
maurolibanore <maurolibanore10@gmail.com>
2026-08-02 23:15:07 -0300

    feat: adicionando primeReact

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// PrimeReact
+import 'primereact/resources/themes/lara-light-purple/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
