1a9be49f37195decd5aca82a2ce9d39fc9aaa443
maurolibanore <maurolibanore10@gmail.com>
2026-08-07 14:20:10 -0300

    refactor: RotaProtegida incluida nas rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Login from './pages/Login/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
 import RecuperacaoSenha from './pages/RecuperacaoSenha/RecuperacaoSenha';
 import RedefinirSenha from './pages/RedefinirSenha/RedefinirSenha';
+import AlteracaoSenha from './pages/AlteracaoSenha/AlteracaoSenha';
 
 function App() {
   return (
@@ -23,13 +24,24 @@ function App() {
           <Route path="/login" element={<Login />} /> 
 
           {/* Rota da tela de dashboard*/}
-          <Route path="/app/dashboard" element={<Dashboard />} /> 
+          <Route path="/app/dashboard" element={
+            <RotaProtegida>
+              <Dashboard />
+            </RotaProtegida>
+          } />
 
           {/* Rota da tela de recSenha*/}
           <Route path="/recuperar-senha" element={<RecuperacaoSenha />} /> 
 
           {/* Rota da tela de redSenha*/}
-          <Route path="/redefinir-senha/:token" element={<RedefinirSenha />} />          
+          <Route path="/redefinir-senha/:token" element={<RedefinirSenha />} />
+
+          {/* Rota da tela de alterarSenha*/}
+          <Route path="/app/perfil/senha" element={
+            <RotaProtegida>
+              <AlteracaoSenha />
+            </RotaProtegida>
+          } />
 
         </Routes>
       </BrowserRouter>      
